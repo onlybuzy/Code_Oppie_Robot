1e677582088f80c2ac3b332d7e6f563deec2ffa2
only_busy <jazuniga19@outlook.com>
2023-09-11 14:09:33 -0500

    1

--- Battery_Level_Indicator.py
+++ Battery_Level_Indicator.py
@@ -1,3 +1,18 @@
-# Create an ADS1115 ADC (16-bit) instance.
-#test
+import time
+import Adafruit_ADS1x15
 
+adc=Adafruit_ADS1x15.ADS1115()
+
+G=2/3
+
+adc.start_adc(0,gain=G)
+
+print("ADC Reading Value: ")
+
+
+
+while True:
+
+    value=adc.get_last_result()
+    print('Channel 0: {0}'.format(value))
+    time.sleep(0.5)
