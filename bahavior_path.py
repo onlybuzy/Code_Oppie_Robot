7643b7bc6491c7b39d8ca51e9fda5d2b68ef4801
only_busy <jazuniga19@outlook.com>
2023-09-24 10:20:34 -0500

    change names

--- bahavior_path.py
+++ bahavior_path.py
@@ -1,4 +1,4 @@
-import Robot_Movement
+import Robot
 from time import sleep
 
 def straight(bot,seconds):
@@ -25,7 +25,7 @@ def spin_right(bot,seconds):
     bot.set_right(-60)
     sleep(seconds)
 
-bot = Robot_Movement.robot_movement()
+bot = Robot.robot_movement()
 straight(bot, 1)
 turn_left(bot,3.3)
 spin_right(bot,0.63)
--- behavior_line.py
+++ behavior_line.py
@@ -1,9 +1,9 @@
 from PCA9685 import PCA9685
 from time import sleep
 
-import Robot_Movement
+import Robot
 
-r=Robot_Movement.robot_movement()
+r=Robot.robot_movement()
 
 r.set_left(50)
 r.set_right(50)
