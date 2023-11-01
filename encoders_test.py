27e087b0d52bbfbd44976f63897a2664fa18045a
only_busy <jazuniga19@outlook.com>
2023-11-01 17:08:33 -0500

    T

--- encoders_test.py
+++ encoders_test.py
@@ -4,40 +4,34 @@ import logging
 
 from gpiozero import DigitalInputDevice
 
-
+logger = logging.getLogger("test_encoders")
 
 class EncoderCounter(object):
-    def __init__(self, pin):
-        self._value = 0
-        encoder = DigitalInputDevice(pin)
-        encoder.when_activated = self._increment
-        encoder.when_deactivated = self._increment
-
-    def reset(self):
-        self._value = 0
-
-    def _increment(self):
-        self._value += 1
-    
-    @property
-    def value(self):
-        return self._value
+   
+   def __init__(self, pin_number):
+        self.pulse_count = 0
+        self.device = DigitalInputDevice(pin=pin_number)
+        self.device.pin.when_changed = self.when_changed
+   def when_changed(self, time_ticks, state):
+        self.pulse_count += 1
 
 
 r=Oppie_Bot.motors_move()
 
 
-enc_1=EncoderCounter(4)
+left_encoder = EncoderCounter(4)
 
-enc_2=EncoderCounter(17)
+right_encoder = EncoderCounter(26)
 
 
-stop_at_time = time.time() + 2
 
+stop_at_time = time.time() + 1
+
+logging.basicConfig(level=logging.INFO)
 
 r.set_left(50) 
 r.set_right(50)
 
 while time.time()<stop_at_time:
-     print(enc_1.value)
-     time.sleep(0.05)
+    logger.info(f"Left: {left_encoder.pulse_count} Right: {right_encoder.pulse_count}")
+    time.sleep(0.05)
