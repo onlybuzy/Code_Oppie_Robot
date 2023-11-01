3205e5a0caedb341c26891359cd6593c1970d072
only_busy <jazuniga19@outlook.com>
2023-11-01 13:38:33 -0500

    encoders test

---
+++ encoders_test.py
@@ -0,0 +1,31 @@
+import Oppie_Bot
+import time
+import logging
+
+from gpiozero import DigitalInputDevice
+
+
+logger=logging.getLogger("test_encoders")
+
+class EncoderCounter(object):
+    def __init__(self, pin_number):
+        self.pulse_count = 0
+        self.device = DigitalInputDevice(pin=pin_number)
+        self.device.pin.when_changed = self.when_changed
+    def when_changed(self, _, state):
+        self.pulse_count += 1
+    
+
+r=Oppie_Bot.motors_move()
+
+enc_1=EncoderCounter(22)
+
+stop_at_time = time.time() + 1
+
+logging.basicConfig(level=logging.INFO)
+r.set_left(50)
+r.set_right(50)
+
+while time.time()<stop_at_time:
+    logger.info(f"ENC: {enc_1.pulse_count} ")
+    time.sleep(0.05)
